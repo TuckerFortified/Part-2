a8c88a9c4cdab0b79fafcf87a968e794bfaccfe1
fortietu <fortietu@OA-S141-08>
2024-02-07 13:50:18 -0500

    Made the Knight move to where the player clicks the mouse.

---
+++ Assets/Week 5/Scripts/Knight.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 destination;
+    Vector2 movement;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        movement = destination - (Vector2)transform.position;
+        if(movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+}

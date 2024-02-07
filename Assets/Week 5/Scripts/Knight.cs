0b29baa3930dd61529a513b4d5570addf168e85e
fortietu <fortietu@OA-S141-08>
2024-02-07 14:04:21 -0500

    Added Walking animations to the Knight. I deviated from the instructions a little and added blend time to the animations are smoother

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -9,20 +9,23 @@ public class Knight : MonoBehaviour
     Vector2 destination;
     Vector2 movement;
     public float speed;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         movement = destination - (Vector2)transform.position;
-        if(movement.magnitude < 0.1)
+        if(movement.magnitude < 0.1) 
         {
             movement = Vector2.zero;
         }
         rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+
         
     }
     
@@ -34,5 +37,6 @@ public class Knight : MonoBehaviour
         {
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
+        animator.SetFloat("Movement", movement.magnitude);
     }
 }

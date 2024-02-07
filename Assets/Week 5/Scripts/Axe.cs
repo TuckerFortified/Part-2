5ca7dfb79e74bfb0c6df46a4701395233a6c887f
fortietu <fortietu@OA-S213-03>
2024-02-07 16:35:38 -0500

    Made an Axe prefab, that damages the player on collision.

---
+++ Assets/Week 5/Scripts/Axe.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    GameObject game;
+    public float time;
+    public float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        game = rb.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time >= timer) 
+        {
+            Destroy(game);
+        }
+        time = time + Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.SendMessage("TakeDamage", 1);
+        Destroy(game);
+    }
+}

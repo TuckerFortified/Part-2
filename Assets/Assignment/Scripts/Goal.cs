fbbcd1dbf8d17b42e005609703d0589dd65a38f5
Tucker Fortier <tucker.fortier@gmail.com>
2024-02-16 11:28:32 -0500

    Made the goal move every time the player makes contact with it. Useed the animation curve to smoothly move the goal.

--- Assets/Assignment/Scripts/Goal.cs
+++ Assets/Assignment/Scripts/Goal.cs
@@ -6,20 +6,62 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    public Transform startPos;
-    public Transform endPos;
+    Transform startPos;
+    public Transform endPos1;
+    public Transform endPos2;
+    public Transform endPos3;
+    public Transform endPos4;
+    public Transform endPos5;
+    Transform endPos;
+    public AnimationCurve animationCurve;
+    public float lerpTimer;
+    public float interpolation;
+    bool isMoving;
 
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         //This code makes the object start at a random position
         transform.position = new Vector3(Random.Range(-8, 8), Random.Range(-4, 4), 0);
     }
 
-    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //This code resets the animation curve and changes the move coordinates
+        startPos = transform;
+        int count = Random.Range(1, 5);
+        if (count == 1)
+        {
+            endPos = endPos1;
+        }
+        else if (count == 2)
+        {
+            endPos = endPos2;
+        }
+        else if (count == 3)
+        {
+            endPos = endPos3;
+        }
+        else if (count == 4)
+        {
+            endPos = endPos4;
+        }
+        else if (count == 5)
+        {
+            endPos = endPos5;
+        }
+
+        //This line resets the animation curve, reseting the animation for moving.
+        lerpTimer = 0;
+    }
+
+    
     void Update()
     {
-        
+        //This code makes the animation curve move
+        interpolation = animationCurve.Evaluate(lerpTimer);
+        transform.position = Vector3.Lerp(startPos.position, endPos.position, interpolation);
+        lerpTimer = lerpTimer + Time.deltaTime;
     }
 }

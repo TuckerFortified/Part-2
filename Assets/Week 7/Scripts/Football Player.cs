c1d437c03106c5d580495480a2ae2350912e0cda
fortietu <fortietu@OA-S141-08>
2024-02-21 14:16:47 -0500

    Made it so the player can be selected.

---
+++ Assets/Week 7/Scripts/Football Player.cs
@@ -0,0 +1,46 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+
+
+public class FootballPlayer : MonoBehaviour
+{
+    public Color Select;
+    public Color Deselect;
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    private void OnMouseDown()
+    {
+         Selected(true);
+        
+
+    }
+
+
+    public void Selected(bool IsSelected)
+    {
+        if (IsSelected) 
+        {
+            spriteRenderer.color = Select;
+        }
+
+        if (IsSelected == false)
+        {
+            spriteRenderer.color = Deselect;
+        }
+    }
+}

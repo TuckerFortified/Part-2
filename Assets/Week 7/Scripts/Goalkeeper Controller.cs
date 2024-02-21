512ed8ce0eb61d4af8e7aafa829b3304d4861c77
fortietu <fortietu@OA-S141-08>
2024-02-21 15:57:28 -0500

    Committing so that I can pull from another computer

---
+++ Assets/Week 7/Scripts/Goalkeeper Controller.cs
@@ -0,0 +1,22 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalkeeperController : MonoBehaviour
+{
+    float GaolDistance = 2;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.transform.position = (Vector2)CurrentSelection.transform.position;
+    }
+}
